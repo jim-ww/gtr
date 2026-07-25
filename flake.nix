90fa77fb065361ab0a9f51800d4fc8d634c25a55
jim-ww <jim.w2610@proton.me>
2026-07-25 12:55:41 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "gtr - Go Translate CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "gtr";
+          version = "0.1.0";
+          src = ./.;
+          vendorHash = null;
+        };
+      });
+}
